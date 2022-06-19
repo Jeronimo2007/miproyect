51d52e37a9b62d867d01d357aa659d0374360c81
Jeronimo Gomez <jeronimogomez0109@gmail.com>
2022-06-19 16:51:43 -0500

    segunda version

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*const wrapper = document.querySelector(".wrapper")
+let header = document.querySelector("header")
+
+
+function onDrag(movementX,movementY){
+    let getStyle = window.getComputedStyle(wrapper)
+    let left = parseInt(getStyle.left);
+    let top = parseInt(getStyle.top);
+    console.log(typeof left, typeof top)
+    wrapper.style.left = `${left + movementX}px`
+    wrapper.style.top = `${top + movementy}px`
+
+}
+
+header.addEventListener("mousedown", function(){
+    header.addEventListener("mousemove", onDrag)
+
+})*/
