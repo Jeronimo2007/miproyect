8af8f849429eca59c9bab062741120ae4371ba83
Jeronimo Gomez <jeronimogomez0109@gmail.com>
2022-06-23 21:16:11 -0500

    guardando cambios

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,57 +1,9 @@
-let addToDoButton = document.querySelector('#addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
-let checkBox = document.getElementsByClassName('checkbox')
-let removeToDoButton = document.getElementById('removeToDo')
-let inputs = document.getElementsByClassName('input')
+var list = document.getElementById()
+var addBtn = document.getElementById()
+var addInput = document.getElementById()
 
-inputField.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        var paragraph = document.createElement('input');
-        paragraph.classList.add('input')
-
-        var buttonCheck = document.createElement('input');
-        buttonCheck.type = 'checkbox'
-        buttonCheck.classList.add('checkbox')
-        buttonCheck.classList.add('checkbox')
+function createTodo () {
+    console.log('')
 
 
-        paragraph.defaultValue = inputField.value;
-
-        toDoContainer.appendChild(paragraph);
-        inputField.value = "";
-
-        let check = checkBox.checked
-        
-        if (check === true) {
-            paragraph.style.textDecoration = 'line through'
-
-        }else{
-            paragraph.style.textDecoration = 'initial'
-
-        }
-
-        removeToDoButton.addEventListener('click', () => {
-            toDoContainer.removeChild(paragraph);
-        })
-
-        if (inputs.value == ''){
-            removeChild(paragraph,buttonCheck)
-        
-        }
-        
-    }
-})       
-
-
-
-function addElement(tag, className) {
-    let element = document.createElement(tag)
-    
-    element.classList.add(className)
-
-
-
-}
-
-console.log({inputField})
+}                   
